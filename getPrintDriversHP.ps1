198d79bfbbebaffab0dee321934c7542668642ff
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2020-06-30 11:38:43 -0700

    HP Version is working in the same way as Canon. Added .exe for HP.
    We still need to add the drivers to the software\dist.

--- getPrintDriversHP.ps1
+++ getPrintDriversHP.ps1
@@ -1,16 +1,25 @@
-pause
-$cred = Get-Credential
-New-PSDrive -Name "S" -Root "\\software\dist\Install Printers\drivers" -Persist -PSProvider "FileSystem" -Credential $cred
-pause
+$ErrorActionPreference = "Stop"
+$failedMounting = $FALSE
+$cred = Get-Credential -Message "Please enter your OC Username and Password"
+try {
+    New-PSDrive -Name "S" -Root "\\software\dist\Install Printers\drivers" -Persist -PSProvider "FileSystem" -Credential $cred
+}
+catch {
+	$failedMounting = $TRUE
+}
+if ($failedMounting){
+	try{
+		New-PSDrive -Name "S" -Root "\\judah\junk$" -PSProvider "FileSystem" -Credential $cred
+	}
+	catch{
+    		throw "There was an error with your Username and Password. The program will end. Please try again."		
+	}
+    	throw "Make sure to close file explorer and run this program again"
+}
 New-Item 'C:\OCDrivers\drivers' -ItemType directory
-pause
 Copy-Item 'S:\HP Universal Driver_Windows_x64\*' 'C:\OCDrivers\drivers'
-pause
-PNPUtil.exe /add-driver 'C:\OCDrivers\drivers\hpbuio200l.inf' /install
-pause
-Add-PrinterDriver -Name "Nope"
-pause
+PNPUtil.exe /add-driver 'C:\OCDrivers\drivers\hpcu240u.inf' /install
+Add-PrinterDriver -Name "HP Universal Printing PCL 6"
 Remove-PSDrive -Name S
-pause
 Remove-Item 'C:\OCDrivers\drivers' -recurse
-pause
+Write-Warning "Success! The Print Drivers have been added!"
