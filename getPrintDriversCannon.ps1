4ec497e0ed1b5c2e781eebfa1cd3817280f51701
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2020-06-30 10:31:08 -0700

    Removed pauses and checks. Added completion messege. Created a .exe.

--- getPrintDriversCannon.ps1
+++ getPrintDriversCannon.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference = "Stop"
 $failedMounting = $FALSE
-pause
 $cred = Get-Credential
 try {
     New-PSDrive -Name "S" -Root "\\software\dist\Install Printers\drivers" -Persist -PSProvider "FileSystem" -Credential $cred
@@ -18,19 +17,13 @@ if ($failedMounting){
     		throw "Ending Program"		
 	}
 	Write-Warning "Make sure to close file explorer and run this program again"
-    	pause
+	pause
     	throw "Ending Program"
 }
-pause
 New-Item 'C:\OCDrivers\drivers' -ItemType directory
-pause
 Copy-Item 'S:\Generic_Plus_UFRII_v2.20_Set-up_x64\Driver\*' 'C:\OCDrivers\drivers'
-pause
 PNPUtil.exe /add-driver 'C:\OCDrivers\drivers\CNLB0MA64.INF' /install
-pause
 Add-PrinterDriver -Name "Canon Generic Plus UFR II"
-pause
 Remove-PSDrive -Name S
-pause
 Remove-Item 'C:\OCDrivers' -recurse
-pause
+Write-Warning "Success! The Print Drivers have been added!"
